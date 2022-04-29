64ea9d73d5e43eeb43f7e7b126c1595dbf8bb870
Zeming <fangzefunny@gmail.com>
2022-04-29 17:30:18 -0400

    add sig=6

--- chapter4/chapter4.py
+++ chapter4/chapter4.py
@@ -25,6 +25,7 @@ class viz:
     Yellow  = .95 * np.array([220, 175, 106]) / 255
     Purple  = .95 * np.array([108,  92, 231]) / 255
     Palette = [Blue, Red, Green, Yellow, Purple]
+    Greens  = [np.array([8,154,133]) / 255, np.array([118,193,202]) / 255] 
     dpi     = 200
     sfz, mfz, lfz = 11, 13, 16
     lw, mz  = 2.5, 6.5
@@ -47,6 +48,7 @@ def Fig4_3(s_check=10, param_Prior=(0, 8), param_Like=4,
     sig       = param_Like
     J, J_s    = 1/sig**2, 1/sig_s**2
     sig_res   = np.sqrt(J/(J+J_s)**2) 
+    bins = np.linspace(-30, 30, n_bins)
 
     # get PM prediction 
     hat_PM = lambda s: [norm((J*si+J_s*mu)/(J+J_s), sig_res
@@ -55,69 +57,88 @@ def Fig4_3(s_check=10, param_Prior=(0, 8), param_Like=4,
     hat_ML = lambda s:[norm(si, sig).rvs(random_state=rng)
                         for si in tqdm(s, ncols=100)]
 
-    # get s the real stimulus and prediction 
-    s = norm(mu, sig_s).rvs(size=n_samples, random_state=rng)
-    
-    # get two threads
-    pool = ThreadPoolExecutor(max_workers=2)
-    job_ML = pool.submit(hat_ML, s)
-    job_PM = pool.submit(hat_PM, s)
-    shat_ML = job_ML.result()
-    shat_PM = job_PM.result()
-    pool.shutdown()
+    # get the sample data, load the cached if data exists
+    fname = f'{path}/data-sig={param_Like}.csv'
+    if os.path.exists(fname): data = pd.read_csv(fname)
+    else: 
+        # get s the real stimulus and prediction 
+        s = norm(mu, sig_s).rvs(size=n_samples, random_state=rng)
+        
+        # get two threads
+        pool = ThreadPoolExecutor(max_workers=2)
+        job_ML = pool.submit(hat_ML, s)
+        job_PM = pool.submit(hat_PM, s)
+        shat_ML = job_ML.result()
+        shat_PM = job_PM.result()
+        pool.shutdown()
 
-    # construct a dataframe for plotting 
-    bins = np.linspace(-30, 30, n_bins)
-    data = pd.DataFrame(np.vstack([s, shat_ML, shat_PM]).T, 
-                        columns=['s', 'shat_ML', 'shat_PM'])
-    data['s_digit']  = np.digitize(data['s'], bins=bins)
-    data['ML_digit'] = np.digitize(data['shat_ML'], bins=bins) 
-    data['PM_digit'] = np.digitize(data['shat_PM'], bins=bins) 
-    data['s-ML_hat'] = data['shat_ML'] - data['s']
-    data['s-ML_digit'] = np.digitize(data['s-ML_hat'], bins=bins) 
-    data['s-PM_hat'] = data['shat_PM'] - data['s']
-    data['s-PM_digit'] = np.digitize(data['s-PM_hat'], bins=bins) 
+        # construct a dataframe for plotting 
+        data = pd.DataFrame(np.vstack([s, shat_ML, shat_PM]).T, 
+                            columns=['s', 'shat_ML', 'shat_PM'])
+        data['s_digit']  = np.digitize(data['s'], bins=bins)
+        data['ML_digit'] = np.digitize(data['shat_ML'], bins=bins) 
+        data['PM_digit'] = np.digitize(data['shat_PM'], bins=bins) 
+        data['s-ML_hat'] = data['shat_ML'] - data['s']
+        data['s-ML_digit'] = np.digitize(data['s-ML_hat'], bins=bins) 
+        data['s-PM_hat'] = data['shat_PM'] - data['s']
+        data['s-PM_digit'] = np.digitize(data['s-PM_hat'], bins=bins)
+
+        # save data 
+        data.to_csv(fname, index=False) 
 
     # replicat figure 4.3 
     nr, nc = 2, 4
     fig, axs = plt.subplots(nr, nc, figsize=(3*nc, 2.9*nr))
-    kws = ['ML', 'PM']
+    kws   = ['ML', 'PM']
+    svals = [1, 10]
     for i, kw in enumerate(kws):
         ax = axs[i, 0]
         sns.scatterplot(x='s', y=f'shat_{kw}', data=data.loc[:1000, :], 
-                color=viz.Palette[i], s=5, ax=ax)
+                color=viz.Greens[i], s=5, ax=ax)
         sns.lineplot(x=bins , y=bins ,
-                ls='--', color=viz.Palette[i], ax=ax)
+                ls='--', color=viz.Greens[i], ax=ax)
+        ax.axvline(x=svals[0], ls='--', color=[.7]*3)
+        ax.axvline(x=svals[1], ls='--', color=[.5]*3)
         ax.set_xlim([-30, 30])
         ax.set_ylim([-30, 30])
+        ax.set_ylabel(f'{kw} est.')
+        ax.set_xlabel('s')
         # infer p(shat|s=10)
         ax = axs[i, 1]
         sns.histplot(x=f'{kw}_digit', data=data.query(
-            f's_digit=={np.digitize(s_check, bins=bins)}'),
+            f's_digit=={np.digitize(svals[0], bins=bins)}'),
             color=[.7]*3, edgecolor=[1,1,1], binwidth=1, 
-            ax=ax)
+            stat='probability', ax=ax)
+        ax.axvline(x=np.digitize(svals[0], bins=bins), ls='--', color=viz.Greens[i])
+        ax.set_xlabel(f's={svals[0]}, common case')
+        ax.set_ylabel(f'p(shat|s={svals[0]})')
         ax.set_xticks([10, 20, 30, 40, 50])
         ax.set_xticklabels([-20, -10, 0, 10, 20])
         # infer p(s|shat=10)
         ax = axs[i, 2]
-        sns.histplot(x=f's_digit', data=data.query(
-            f'{kw}_digit=={np.digitize(s_check, bins=bins)}'), 
-            color=[.3]*3, edgecolor=[1,1,1], binwidth=1, 
-            ax=ax)
+        sns.histplot(x=f'{kw}_digit', data=data.query(
+            f's_digit=={np.digitize(svals[1], bins=bins)}'), 
+            color=[.5]*3, edgecolor=[1,1,1], binwidth=1, 
+            stat='probability', ax=ax)
+        ax.axvline(x=np.digitize(svals[1], bins=bins), ls='--', color=viz.Greens[i])
+        ax.set_xlabel(f's={svals[1]}, rare case')
+        ax.set_ylabel(f'p(shat|s={svals[1]})')
         ax.set_xticks([10, 20, 30, 40, 50])
         ax.set_xticklabels([-20, -10, 0, 10, 20])
         # show prediction error distribution
         ax = axs[i, 3]
         sns.histplot(x=f's-{kw}_digit', data=data, 
-            color=viz.Palette[i], edgecolor=[1,1,1], binwidth=1,
-            ax=ax)
+            color=viz.Greens[i], edgecolor=[1,1,1], binwidth=1,
+            stat='probability', ax=ax)
         ax.set_xticks([10, 20, 30, 40, 50])
         ax.set_xticklabels([-20, -10, 0, 10, 20])
+        ax.set_xlabel('s - shat')
+        ax.set_ylim([0, .12])
     fig.tight_layout()
     plt.savefig(f'{path}/Fig_4_3_sig={param_Like}.png', dpi=viz.dpi)
 
 if __name__ == '__main__':
 
-    Fig4_3(param_Like=(8), n_samples=1000000)
+    Fig4_3(param_Like=(4), n_samples=1000000)
         
 
